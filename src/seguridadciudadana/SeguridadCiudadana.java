a1dd5782324ddf092cdf3e6b84372644cd23559c
GianellaAnchivilca19 <142622889+GianellaAnchivilca19@users.noreply.github.com>
2024-07-04 14:45:35 -0500

    Add files via upload

---
+++ src/seguridadciudadana/SeguridadCiudadana.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package seguridadciudadana;
+
+import Pantallas.Inicio;
+
+/**
+ *
+ * @author giane
+ */
+public class SeguridadCiudadana {
+
+    public static void main(String[] args) {
+        
+        Inicio ini = new Inicio ();
+        ini.setVisible(true);
+        
+        ini.setLocationRelativeTo(null);
+        
+    }
+    
+}
